24fbbe2fc63fb9e23df41ba9e9e923319be314b7
DonovanDudley <132828594+DonovanDudley@users.noreply.github.com>
2023-05-07 06:49:46 -0700

    Add files via upload

---
+++ hello-git.py
@@ -0,0 +1,4 @@
+def HelloFunc():
+    russian = "Привет ГитХаб! Это мой первый скрипт в моём первом репозитории!)"
+    english = "Hello GitHub! This is my first script in my first repository!)"
+    frensh = "Bonjour Githab! Ceci est mon premier script dans mon premier dépôt!)"
